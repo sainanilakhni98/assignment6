6cc3ba10a65389f55c6f16d2cdf3514f943fe3ad
Yogesh sainani <44187790+sainanilakhni98@users.noreply.github.com>
2021-06-07 09:00:48 +0530

    Delete EmployeeManagementApp directory

--- EmployeeManagementApp/src/main/java/com/nagarro/EmployeeManagementApp/dao/EmployeeDao.java
+++
@@ -1,9 +0,0 @@
-package com.nagarro.EmployeeManagementApp.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.nagarro.EmployeeManagementApp.Employee;
-
-public interface EmployeeDao extends JpaRepository<Employee,Integer>
-{
-
-}
