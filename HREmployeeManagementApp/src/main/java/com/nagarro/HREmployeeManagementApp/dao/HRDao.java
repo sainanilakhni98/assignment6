d0721302390ca1fbcc59cb768fbb265ab08ee035
Yogesh sainani <44187790+sainanilakhni98@users.noreply.github.com>
2021-06-07 09:01:16 +0530

    Delete HREmployeeManagementApp directory

--- HREmployeeManagementApp/src/main/java/com/nagarro/HREmployeeManagementApp/dao/HRDao.java
+++
@@ -1,9 +0,0 @@
-package com.nagarro.HREmployeeManagementApp.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nagarro.HREmployeeManagementApp.entity.HRDetail;
-
-public interface HRDao extends JpaRepository<HRDetail,Integer> {
-
-}
