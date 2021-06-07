2c1fe423496f98e56115ea2ae1d018587262d3e3
Yogesh sainani <44187790+sainanilakhni98@users.noreply.github.com>
2021-06-07 09:01:03 +0530

    Delete Exercise1 directory

--- Exercise1/Exercise3.sql
+++
@@ -1,3 +0,0 @@
--------------------------Exercise 3----------------
-select top(5) SalesOrderID,AccountNumber,TotalDue from Sales.SalesOrderHeader so
-where so.TotalDue>70000 order by OrderDate desc,TotalDue desc;
